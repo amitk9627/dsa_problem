88ef6b00b01631edc5cbd2ede4d5aaccc4edd9b9
Amit kumar <120603787+amitk9627@users.noreply.github.com>
2024-03-12 23:33:07 +0530

    Prime Factorisation

--- src/PrimeFactors.java
+++ src/PrimeFactors.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class PrimeFactors {
     public static void main(String[] args) {
-        int num=10;
-        for(int i=2;i<num;i++){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        for(int i=2;i*i<num;i++){
             while(num%i==0){
                 System.out.println(i);
                 num=num/i;
             }
         }
+        if(num!=1){
+            System.out.println(num);
+        }
+        sc.close();
     }
 }
