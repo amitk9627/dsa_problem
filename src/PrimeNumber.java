58cc3a2c251dafe27407b3f3d03e0592b1e366f8
Amit kumar <120603787+amitk9627@users.noreply.github.com>
2024-07-02 22:39:54 +0530

    Update PrimeNumber.java
    remove the comment

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -4,22 +4,6 @@ public class PrimeNumber {
         Scanner sc=new Scanner(System.in);
         int num=sc.nextInt();
         boolean bool=false;
-//         O(n)
-//        for(int i=2;i<num;i++){
-//            if(num%i==0){
-//                bool=true;
-//                break;
-//            }
-//        }
-
-//        O(n/2)
-//        for(int i=2;i<(num/2);i++){
-//            if(num%i==0){
-//                bool=true;
-//                break;
-//            }
-//        }
-
         for(int i=2;i<Math.sqrt(num);i++){
             if(num%i==0){
                 bool=true;
